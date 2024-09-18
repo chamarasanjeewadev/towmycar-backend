27d1e02ebd665631829854a84beedf58f32eb620
Chamara Sanjeewa <chamara.sanjeewa.uk@gmail.com>
2024-09-18 11:50:06 +0100

    Remove db-data files from Git tracking and update .gitignore

--- notification_service/src/utils/firebase-admin.ts
+++ notification_service/src/utils/firebase-admin.ts
@@ -1,10 +1,10 @@
-import admin from 'firebase-admin';
+ import admin from 'firebase-admin';
 
-// Replace with the path to your service account key file
-const serviceAccount = require('./serviceAccount.json');
+// // Replace with the path to your service account key file
+// const serviceAccount = require('./serviceAccount.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 export default admin;
